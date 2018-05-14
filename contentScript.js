2cacc6b5f7f12407e535ff654e0483d5719b99ac
sjf <sjf@juicyminds.cc>
2018-05-14 20:07:14 +0800

    基本技术要素完成

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,28 @@
-document.body.style.backgroundColor = "#FF00FF";
+//背景设为紫色
+//document.body.style.backgroundColor = "#FF00FF";
 //var clip = window.clipboardData.getData("Text")
 
 document.addEventListener('paste', function (event) {
     var clipText = event.clipboardData.getData('Text');
     alert('pasting:' + clipText)
-});
+    console.log(clipText)
+    console.log("asdf")
+})
 
+var accountInputs = document.querySelectorAll('.account-display');
+console.log(accountInputs)
+
+
+for (var i =0; i<accountInputs.length; i++) {
+    accountInputs[i].value = 'aa'
+}
+
+var addUserButton = document.querySelectorAll('a#addUser.add-pointer')[0]
+console.log(typeof(addUserButton))
+for (var names in addUserButton) {
+    console.log(names)
+}
+console.log(addUserButton.onclick)
+for (var i = 0; i<18; i++)
+    // TODO：console中onclick可以调用。此处调用onclick不行，只能是click。原因不明。
+    addUserButton.click()
